ed20596810c72a4072b75c2aa51594cb0e8e54ff
Bruno Coelho <bbcoelho@gmail.com>
2024-07-03 17:15:06 -0300

    chore: src folder

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import figlet from "figlet";
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-        const body = figlet.textSync("Bun!");
+        const body = figlet.textSync("Brunão");
         return new Response(body);
         return new Response("Bun!");
     },
