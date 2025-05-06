c87cf0ddb1c1d6ab5dda2fd7d0960abddf0320a2
Bruno Coelho <bbcoelho@gmail.com>
2025-05-05 19:54:47 -0700

    test: UrlFetchApp

--- src/Controller.ts
+++ src/Controller.ts
@@ -1,22 +1,10 @@
 function doGet() {
-  const sheet = SpreadsheetApp.getActiveSheet();
-  const numRows = 10; // Number of rows to fill
-  const numCols = 10; // Number of columns to fill
-  
-  // Create a 2D array to hold all the values
-  const values: string[][] = [];
-  
-  // Fill the array with values representing column and row positions
-  for (let row = 1; row <= numRows; row++) {
-    const rowValues: string[] = [];
-    for (let col = 1; col <= numCols; col++) {
-      rowValues.push(`R${row}C${col}`);
-    }
-    values.push(rowValues);
-  }
-  
-  // Set all values at once for better performance
-  sheet.getRange(1, 1, numRows, numCols).setValues(values);
-  
-  return ContentService.createTextOutput('Spreadsheet updated successfully');
+	const spreadSheet = SpreadsheetApp.openById('1NRH4u6tyr5RJPwq1tkvKXuRAnLPAMSTVC8T2YVRCsHY');
+	const sheet = spreadSheet.getSheetByName('Sheet1');
+	try {
+		const response = UrlFetchApp.fetch('https://global.awxbio.cool/remote/glycemic/api/107462?start_time=1746153818834&end_time=1746240218834&openid=e24d053ec64449a2ee4b75f9e55d1987');
+		console.log(response.getContentText());
+	} catch (error) {
+		console.log(error);
+	}
 }
