a0e9bb8fe55a82a0b9c88d063af1f8a146daa4cf
Bruno Coelho <bbcoelho@gmail.com>
2025-05-08 11:18:28 -0700

    feat: fetch data

--- src/Controller.ts
+++ src/Controller.ts
@@ -3,7 +3,7 @@ function doGet() {
 	const sheet = spreadSheet.getSheetByName('Sheet1');
 	try {
 		const response = UrlFetchApp.fetch('https://global.awxbio.cool/remote/glycemic/api/107462?start_time=1746153818834&end_time=1746240218834&openid=e24d053ec64449a2ee4b75f9e55d1987');
-		console.log(JSON.stringify(response.getContentText()));
+		console.log(JSON.parse(response.getContentText()).data);
 	} catch (error) {
 		console.log(error);
 	}
