d3dc0b6eb1b22353d3f61ec8dd3cd2d0d413ee76
Bruno Coelho <bbcoelho@gmail.com>
2025-05-10 09:22:55 -0700

    feat: add DataPoint class to Model

--- src/App.ts
+++ src/App.ts
@@ -6,15 +6,14 @@ function doGet() {
 		throw new Error('Sheet not found');
 	}
 
-	sheet.clear();
+	sheet.clearContents();
 
 	try {
-		const response = UrlFetchApp.fetch('https://global.awxbio.cool/remote/glycemic/api/110012?start_time=1746679211520&end_time=1746765611520&openid=c65d3d607944ed92f4e4f4893cfe4a12');
-		const data = JSON.parse(response.getContentText()).data.glycemic_list as Model.rawData[];
+		const response = UrlFetchApp.fetch('https://global.awxbio.cool/remote/glycemic/api/110012?start_time=1746807176845&end_time=1746893576845&openid=c65d3d607944ed92f4e4f4893cfe4a12');
+		const data = JSON.parse(response.getContentText()).data.glycemic_list;
 		for (const item of data) {
-			const time = new Date(item.time);
-			const glycemic = item.glycemic * 18.018;
-			sheet.appendRow([time, glycemic]);
+			const dataPoint = new Model.DataPoint(item).getData();
+			sheet.appendRow([dataPoint.time, dataPoint.glycemic]);
 		}
 	} catch (error) {
 		console.log(error);
--- src/Model.ts
+++ src/Model.ts
@@ -1,9 +1,37 @@
 namespace Model {
 
-    export type rawData = {
+    type rawData = {
         glycemic: number;
         time: number;
         range: number;
+    }
+
+    type dataPoint = {
+        time: Date;
+        glycemic: number;
+    }
+
+    export class DataPoint {
+
+        private data: dataPoint;
+
+
+        constructor(data: rawData) {
+            this.data = this.parseData(data);
+        }
+
+
+        getData(): dataPoint {
+            return this.data;
+        }
+
+        private parseData(data: rawData): dataPoint {
+            return {
+                time: new Date(data.time),
+                glycemic: data.glycemic * 18.018
+            }
+        }
+
     }
 
 }
