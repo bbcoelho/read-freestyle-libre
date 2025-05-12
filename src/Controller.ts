611ea7c0e7ce640605c78403c63f6a2748273687
Bruno Coelho <bbcoelho@gmail.com>
2025-05-11 21:20:04 -0700

    feat: store lastEndTime for later retrieval

--- src/Controller.ts
+++ src/Controller.ts
@@ -1,15 +1,27 @@
 function doGet() {
 	const SPREAD_SHEET_ID = '1NRH4u6tyr5RJPwq1tkvKXuRAnLPAMSTVC8T2YVRCsHY';
-	const SHEET_NAME = 'Leituras';
+	const SHEET_NAME = 'TESTE';
 
 	const sheet = View.getSheet(SPREAD_SHEET_ID, SHEET_NAME);
 
 	// get last 24 hours in milliseconds
-	const endTime = Date.now();
-	const startTime = endTime - (1000 * 60 * 60 * 24);
+	let startTime: number;
+	let endTime: number;
+
+	let lastEndTime = PropertiesService.getScriptProperties().getProperty('lastEndTime');
+	if (!lastEndTime) {
+		endTime = Date.now();
+		startTime = endTime - (1000 * 60 * 60 * 24);
+	} else {
+		startTime = parseInt(lastEndTime);
+		endTime = startTime + (1000 * 60 * 60 * 24);
+	}
 
 	const dataPoints = Model.fetchData(startTime, endTime);
 	if (dataPoints) {
 		View.render(dataPoints, sheet);
 	}
+
+	// stores the last endTime
+	PropertiesService.getScriptProperties().setProperty('lastEndTime', endTime.toString());
 }
